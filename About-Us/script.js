ae22bc19063ccb6d6237525a627a8cf7b5a9db47
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-03-26 13:48:46 -0400

    Update script.js

--- About-Us/script.js
+++ About-Us/script.js
@@ -1,25 +1,15 @@
 let a = prompt("Algebra-For-Kids.w3space.com is not responding. Do you 
-want to quit? (y/n)");
-const websites= ["","","","",""];
+want to quit?","(y/n)");
 
 document.getElementById("header").innerHTML = "About Us";
 
 let cookie = document.cookie;
-cookie = cookie.slice(12,24);
 
-
-if (a =="alex-smells") {
-    if (cookie=="alex-smells") {
+if (a =="alex-smells" & cookie="alex-smells") {
         document.getElementById("header").outerHTML = "";
-        document.getElementById("main").outerHTML = "<iframe id='frame' 
-src='https://mathfun18.dcmusic.ca/' width=' " + window.innerWidth + "' 
-height='" + window.innerHeight + "' allowfullscreen='true' 
-target='_self'></iframe>";
-    }
+        document.getElementById("main").outerHTML = "<iframe id='frame' src='https://mathfun18.dcmusic.ca/' width=' " + window.innerWidth + "' height='" + window.innerHeight + "' allowfullscreen='true' target='_self'></iframe>";
 } else if (a=="y") {
     close();
 } else {
-    document.getElementById("main").innerHTML = "<p 
-style='text-align:center'>Sorry, This content is unavailable. </p>" + 
-cookie + "|";
+    document.getElementById("main").innerHTML = "<p style='text-align:center'>Sorry, This content is unavailable.</p>";
 }
