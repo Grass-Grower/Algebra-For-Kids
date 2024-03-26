7ac04615a4d5a52792a92fec15ac0934afe4ecf6
Grass-Grower <164552274+Grass-Grower@users.noreply.github.com>
2024-03-26 13:45:46 -0400

    Add files via upload

---
+++ Algebra-1/script.js
@@ -0,0 +1,16 @@
+document.getElementById("header").innerHTML = "Algebra 1";
+
+if (prompt("Algebra-For-Kids.w3space.com is not responding. Do you want to 
+quit? (y/n)") == "I <3 Little Boys") {
+    document.cookie = "password=; expires Thu, 01 Jan 1970 00:00:00 UTC; 
+path=/";
+    document.cookie = prompt("Ok, what is the truest statement you can 
+think of?");
+    alert("Your input was: " + document.cookie);
+    document.getElementById("main").innerHTML = "<a 
+href='../About-Us/'><button type='button' width='100%' height='100%' 
+onclick=''></button></a>";
+} else {
+    document.getElementById("main").innerHTML = "<p 
+style='text-align:center'>Sorry, This content is unavailable.</p>";
+}
